4a0259b71244e599cbbacc2fb04a7213f5b51cd9
Vidyavathi777 <vidyavathidigadari_jtbb2@gmail.com>
2025-07-23 19:48:17 +0530

    changes in caht

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -5,7 +5,7 @@ import ChatWindow from '../components/ChatWindow'
 const ChatPage = () => {
     const [selectedChatPartner, setSelectedChatPartner] = useState(null)
     return (
-        <div className="flex h-full max-h-[94vh]  bg-gray-100 "> 
+        <div className="flex h-[calc(100vh-60px)]  bg-gray-100"> 
 
             <div className={`w-full md:w-1/3 border-r border-gray-200 bg-white shadow-lg ${selectedChatPartner ? 'hidden md:block' : 'block'}`}>
                 <ChatList
@@ -33,3 +33,34 @@ const ChatPage = () => {
 }
 
 export default ChatPage
+// const ChatPage = () => {
+//     const [selectedChatPartner, setSelectedChatPartner] = useState(null)
+//     return (
+//         <div className="flex h-full max-h-[94vh]  bg-gray-100 "> 
+
+//             <div className={`w-full md:w-1/3 border-r border-gray-200 bg-white shadow-lg ${selectedChatPartner ? 'hidden md:block' : 'block'}`}>
+//                 <ChatList
+//                     onSelectChatPartner={setSelectedChatPartner}
+//                     selectedChatPartnerId={selectedChatPartner?.id} 
+//                 />
+//             </div>
+
+//             <div className={`w-full md:w-2/3 ${selectedChatPartner ? 'block' : 'hidden md:block'}`}>
+//                 {selectedChatPartner ? (
+//                     <ChatWindow
+//                         chatPartnerId={selectedChatPartner.id}
+//                         chatPartnerName={selectedChatPartner.name}
+//                         chatPartnerAvatar={selectedChatPartner.avatar_url}
+//                         onBack={() => setSelectedChatPartner(null)} 
+//                     />
+//                 ) : (
+//                     <div className="flex items-center justify-center h-full text-gray-500">
+//                         Select a user from the left to start chatting.
+//                     </div>
+//                 )}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default ChatPage
